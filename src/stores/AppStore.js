9d369372fbc3654112c18b42a1f36b32bfb38bbb
Mark Carter <mark4carter@gmail.com>
2015-11-02 16:17:35 -0500

    JSONP call to store

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,18 +1,37 @@
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+var AppConstants = require('../constants/AppConstants');
 
 
 var CHANGE_EVENT = 'change';
 
+var pulledData;
+
 var AppStore = assign({}, EventEmitter.prototype, {
   emitChange: function() {
     this.emit(CHANGE_EVENT);
+  },
+
+
+  addChangeListener: function(callback) {
+  	this.on(CHANGE_EVENT, callback);
+  },
+
+  getPulledData: function() {
+  	return pulledData;
   }
 });
 
 AppDispatcher.register(function(payload){
-  console.log(payload);
+
+  var actionz = payload.action;
+
+  if (actionz.actionType == AppConstants.PULL_DATA){
+  	pulledData = actionz.item;
+  	console.log(pulledData);
+  }
+  AppStore.emitChange();
   return true;
 });
 
